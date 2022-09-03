0f3c8eb266b2f691868f58acdec8c42687584fb3
mohyamin <mohyamin18@gmail.com>
2022-09-03 13:24:28 +0700

    Merubah isi File Html

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["fresh graduate", "Junior Software Engineer", "Designer"],
+        strings: ["fresh graduate", "Junior Software Engineer", "Designer", "IT Support"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["fresh graduate", "Junior Software Engineer", "Designer"],
+        strings: ["fresh graduate", "Junior Software Engineer", "Designer", "IT Support"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
